ce8f10aa57b076e7e4f4ea21c8aa9557d7744897
Anjali Choudhary <anjalichoudhary1999.ac@gmail.com>
2019-10-01 02:25:26 +0530

    initial

--- designs.js
+++ designs.js
@@ -3,8 +3,37 @@
 
 // When size is submitted by the user, call makeGrid()
 
+document.getElementById('btn').addEventListener('click',makeGrid);
+document.getElementById('clrbtn').addEventListener('click',changecolor);
+let col;
+function changecolor()
+{
+    col = document.getElementById('colorPicker').value;
+}
+
 function makeGrid() {
 
 // Your code goes here!
-
+        event.preventDefault();
+        let tb1 = document.createElement('table');
+        let tb1body = document.createElement('tbody');
+        let h = document.getElementById('inputHeight').value;
+        let w = document.getElementById('inputWidth').value;
+        col = document.getElementById('colorPicker').value;
+        for(let i = 0; i < h; i++)
+        {
+            let row = document.createElement('tr');
+            for(let j = 0; j < w; j++)
+            {
+                let cell = document.createElement('td');
+                cell.addEventListener('click',function(e)
+                {
+                    cell.style.backgroundColor = col;
+                });
+                row.appendChild(cell);
+            }
+            tb1body.appendChild(row);
+        }
+        tb1.appendChild(tb1body);
+        document.body.appendChild(tb1);
 }
